49e6a0e54bb70986ac0376f6e1c71c6ae35c5b83
sophietk <sophie.tk@gmail.com>
2014-02-16 20:35:42 +0100

    Manage scores

---
+++ src/main/java/fr/xebia/sophietk/memory/ScoreService.java
@@ -0,0 +1,17 @@
+package fr.xebia.sophietk.memory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreService {
+
+	private Map<String, Integer> scores = new HashMap<String, Integer>();
+
+	public int addTurnScoreAndReturnGameScore(String ip, int gameId, int turnScore) {
+		String key = ip + " " + gameId;
+		int newGameScore = scores.containsKey(key) ? scores.get(key) + turnScore : turnScore;
+		scores.put(key, newGameScore);
+
+		return newGameScore;
+	}
+}
---
+++ src/test/java/fr/xebia/sophietk/memory/ScoreServiceTest.java
@@ -0,0 +1,50 @@
+package fr.xebia.sophietk.memory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ScoreServiceTest {
+
+	public static final String AN_IP = "127.0.0.1";
+	public static final String ANOTHER_IP = "127.0.0.2";
+
+	@Test
+	public void should_increment_score_for_a_player() {
+		ScoreService scoreService = new ScoreService();
+
+		int currentScore = scoreService.addTurnScoreAndReturnGameScore(AN_IP, 1, 10);
+		assertTrue(currentScore == 10);
+
+		currentScore = scoreService.addTurnScoreAndReturnGameScore(AN_IP, 1, 5);
+		assertTrue(currentScore == 15);
+	}
+
+	@Test
+	public void should_decrement_score_several_time_for_a_player() {
+		ScoreService scoreService = new ScoreService();
+
+		scoreService.addTurnScoreAndReturnGameScore(AN_IP, 1, 10);
+		scoreService.addTurnScoreAndReturnGameScore(AN_IP, 1, -3);
+		int currentScore = scoreService.addTurnScoreAndReturnGameScore(AN_IP, 1, -3);
+		assertTrue(currentScore == 4);
+	}
+
+	@Test
+	public void should_not_mix_scores_from_different_players() {
+		ScoreService scoreService = new ScoreService();
+
+		scoreService.addTurnScoreAndReturnGameScore(AN_IP, 1, 10);
+		int currentScoreOtherPlayer = scoreService.addTurnScoreAndReturnGameScore(ANOTHER_IP, 1, -3);
+		assertTrue(currentScoreOtherPlayer == -3);
+	}
+
+	@Test
+	public void should_not_mix_scores_from_different_games() {
+		ScoreService scoreService = new ScoreService();
+
+		scoreService.addTurnScoreAndReturnGameScore(AN_IP, 1, 10);
+		int currentScoreOtherGame = scoreService.addTurnScoreAndReturnGameScore(AN_IP, 2, -3);
+		assertTrue(currentScoreOtherGame == -3);
+	}
+}
