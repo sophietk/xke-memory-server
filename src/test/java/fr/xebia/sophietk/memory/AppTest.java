997a60e96911315baa305542fa08fe7f7bc6b122
sophietk <sophie.tk@gmail.com>
2014-02-16 17:48:05 +0100

    Jersey client for testing

--- src/test/java/fr/xebia/sophietk/memory/AppTest.java
+++ src/test/java/fr/xebia/sophietk/memory/AppTest.java
@@ -1,7 +1,6 @@
 package fr.xebia.sophietk.memory;
 
-import org.jboss.resteasy.client.ClientRequest;
-import org.jboss.resteasy.client.ClientResponse;
+import com.sun.jersey.api.client.Client;
 import org.junit.Test;
 
 import java.io.Closeable;
@@ -10,16 +9,20 @@ import static org.junit.Assert.assertEquals;
 
 public class AppTest {
 
-	public static final int TEST_PORT = 3001;
-	public static final String TEST_APP_ROOT = "http://0.0.0.0:" + TEST_PORT;
+	private static final int TEST_PORT = 3001;
+	private static final String TEST_APP_ROOT = "http://0.0.0.0:" + TEST_PORT;
+
+	private Client client = Client.create();
 
 	@Test
 	public void should_start_server() throws Exception {
 		Closeable server = App.startServer(TEST_PORT);
 
-		ClientResponse<String> response = new ClientRequest(TEST_APP_ROOT + "/play/ping").get(String.class);
-		assertEquals(200, response.getStatus());
-		assertEquals("pong", response.getEntity());
+		String response = client.resource(TEST_APP_ROOT)
+				.path("play/ping")
+				.get(String.class);
+
+		assertEquals("pong", response);
 
 		server.close();
 	}
